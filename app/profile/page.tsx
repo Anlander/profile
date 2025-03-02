61835c6654466b212499aeafbc6c942098474683
anlander <anlanderp@gmail.com>
2025-03-02 23:50:22 +0100

    fixed link

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -4,11 +4,11 @@ import { Navigation } from "@/components/navigation";
 
 const page = () => {
   return (
-    <div className="px-5 my-24   lg:px-14 flex items-center h-full 2xl:h-full">
+    <div className="px-5 my-24 lg:px-14 flex items-center h-full 2xl:h-full">
       <div className="absolute top-5 left-5 justify-center items-center">
         <Navigation home={true} />
       </div>
-      <div className="grid grid-cols-1 gap-5 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4  lg:max-w-[90%] xl:max-w-[85%] 2xl:max-w-[80%] mx-auto">
+      <div className="grid grid-cols-1 gap-5 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4  lg:max-w-[90%] xl:max-w-[95%] 2xl:max-w-[100%] mx-auto">
         {data.map((el: any, i: number) => (
           <div key={i}>
             <CardSection
