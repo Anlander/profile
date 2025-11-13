a9a9a694b5af5bece3af3bd3799268cdf2078c17
anlander <anlanderp@gmail.com>
2025-11-13 15:34:33 +0100

    updated

--- app/layout.js
+++ app/layout.js
@@ -4,8 +4,8 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Website Title',
-  description: 'Website Description',
+  title: 'Philip Anlander',
+  description: 'Welcome my personal website!',
 }
 
 export default function RootLayout({ children }) {
